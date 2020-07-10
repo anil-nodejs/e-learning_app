b590128fa120dc74b1f212161bc02a71c33ea87c
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-10 18:46:43 +0530

    added

--- app.js
+++ app.js
@@ -2,16 +2,27 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+//database
+const db = require('./config/database').mongoDB_URL;
+mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+  if (err) {
+    console.log("Mongodb is not connected");
+  }
+  else {
+    console.log("Mongo db is connected");
+  }
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
---
+++ config/database.js
@@ -0,0 +1,6 @@
+db_Link = 'mongodb+srv://sbycc2020:' + encodeURIComponent('sbycc@anilyadav21') + '@mongodb01-z7hcd.mongodb.net/e_learning?retryWrites=true';
+module.exports =
+{
+    mongoDB_URL: db_Link
+}
+
