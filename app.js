bbcc32b722070d5f5a47f132555cc75f028a2072
Anil Yadav <iter.anilyadav@gmail.com>
2020-07-11 01:22:11 +0530

    added

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
 var logger = require('morgan');
+//new added module
+var expbhs = require('express-handlebars');
+var bodyParser = require('body-parser')
+const expressValidator = require('express-validator')
+var flash = require('connect-flash');
+var session = require('express-session');
+var passport = require('passport');
+var localStrategy = require('passport-local'), Strategy;
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -23,19 +32,55 @@ mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true }, (err) 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.static('views'))
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+//app session
+app.use(session({
+  secret: 'secret',
+  saveUninitialized: true,
+  resave: true
+}))
+
+//passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Express validaor
+app.use(expressValidator({
+  errorFormatter: function (param, msg, value) {
+    var nameSpace = param.split('.'), root = nameSpace.shift(), formParam = root;
+    while (nameSpace.length) {
+      formParam += '[' + nameSpace.shift() + ']';
+    }
+    return { params: formParam, msg: msg, value: value }
+  }
+}))
+
+//connect flash
+app.use(flash());
+
+//Globle vars
+app.use((req, res, next) => {
+  res.locals.message = require('express-messages')(req, res);
+  next();
+});
+
+// Route path
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
